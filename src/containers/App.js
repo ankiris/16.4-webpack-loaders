ec3503e78468165608bf5a9f9c71c8bd11ac09ce
Kat Lasota <lasota.kat@gmail.com>
2018-08-19 14:03:43 +0200

    Add title.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
+import Title from '../components/Title'
 
 
 class App extends React.Component {
@@ -25,7 +26,7 @@ class App extends React.Component {
     render() {
         return (
             <div className={style.TodoApp}>
-                Tutaj pojawią się komponenty naszej aplikacji.
+                    <Title count={this.state.data.length} title={this.props.title}/>
             </div>
         );
     }
