069ac54d7a113cc83f345d39e042a3ec084d5b48
Kat Lasota <lasota.kat@gmail.com>
2018-08-19 16:50:02 +0200

    16.7

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
-import Title from '../components/Title'
-import TodoList from '../components/TodoList'
+import Title from '../components/Title';
+import TodoList from '../components/TodoList';
+import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             data: [{
-                id: 1,
+                    id: 1,
                     text: 'clean room'
                 }, {
-                id: 2,
+                    id: 2,
                     text: 'wash the dishes'
                 }, {
-                id: 3,
+                    id: 3,
                     text: 'feed my cat'
-                }]
+                }, {
+                    id: 4,
+                    text: 'buy butter'
+                }, {
+                    id: 5,
+                    text: 'clean bathroom'
+            }
+            ]
         };
     }
     addTodo(val){
@@ -45,4 +53,4 @@ class App extends React.Component {
     }
 }
 
-export default App;
+export default hot(module)(App);
